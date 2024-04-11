478d965889891488e97cb0de2c411fa7cc12df04
RodrigoDelgado005 <162932332+RodrigoDelgado005@users.noreply.github.com>
2024-04-11 12:08:16 -0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+function CambiarTamañoPequeño(){
+    let tit=document.getElementById('title')
+    tit.style.fontSize ='10px'
+}
+function CambiarTamañoMediano(){
+    let tit=document.getElementById('title')
+    tit.style.fontSize ='13px'
+}
+function CambiarTamañoGrande(){
+    let tit=document.getElementById('title')
+    tit.style.fontSize ='20px'
+}
